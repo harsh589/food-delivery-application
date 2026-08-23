01fc7add9c5dd779c7b0b4cf483a1a23f00663cd
harsh589 <harshyadav1457@gmail.com>
2026-08-24 00:12:11 +0530

    updated

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/3a/d097e242129f00111dbb80c9e21a5180
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+	private String name
+	
+}
--- User-service/src/main/java/com/example/demo/User.java
+++ User-service/src/main/java/com/example/demo/User.java
@@ -1,6 +1,12 @@
 package com.example.demo;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,6 +21,26 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class User {
 
-	private String name
+	
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+	private String name;
+    @Column(nullable = false)
+	private String email;
+    @Column(nullable = false)
+	   private String password;
+	   
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+	   private Role role;
+
+	    public enum Role {
+	        CUSTOMER, RESTAURANT_OWNER, DELIVERY_PARTNER
+	    }
 	
 }

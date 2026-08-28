612b32f35868204123f392a76ce4ba9e457d9789
Yadav <2376688@cognizant.com>
2026-08-28 20:48:50 +0530

    updated

--- User-service/src/main/java/com/example/demo/UserService.java
+++ User-service/src/main/java/com/example/demo/UserService.java
@@ -1,5 +1,19 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+	@Autowired
+	UserRepository repo;
+	
+	@Autowired
+	jwtUtil util;
+	
+	
+	
+	
 
 }

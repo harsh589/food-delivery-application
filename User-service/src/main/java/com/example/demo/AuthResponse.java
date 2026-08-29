653771500e6e5a920551595a1645f8c1f5439a1c
Yadav <2376688@cognizant.com>
2026-08-29 14:27:28 +0530

    updated

--- User-service/src/main/java/com/example/demo/AuthResponse.java
+++ User-service/src/main/java/com/example/demo/AuthResponse.java
@@ -4,13 +4,62 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class AuthResponse {
+
     private Long userId;
     private String name;
     private String email;
     private String role;
     private String token;
+
+    public AuthResponse() {
+    }
+
+    public AuthResponse(Long userId, String name, String email, String role, String token) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.role = role;
+        this.token = token;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+    
+    public String getToken() {
+        return token;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
